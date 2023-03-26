0fd734eb9ab29716994057192a698ebc1d0ddd2b
Reid <reschneyer89@gmail.com>
2023-03-26 12:47:07 -0500

    formatting

--- OSE/PbtaAbilityCheck.js
+++ OSE/PbtaAbilityCheck.js
@@ -1,7 +1,8 @@
 const abilities = ["cha", "con", "dex", "int", "str", "wis"];
+const ab_full_names = {"cha":"Charisma", "con":"Constitution", "dex":"Dexterity", "int":"Intelligence", "str":"Strength", "wis":"Wisdom"};
 let scores = actor.system.scores;
-let dialog_options = abilities.map(a=>`<option value=${a}>${a.toUpperCase()}</option>`).join(``);
-const dialog_content = `<div><h2>Ability Check!</h2></div><div>Select Ability:<select name="ability">${dialog_options}</select></div>`;
+let dialog_options = abilities.map(a=>`<option value=${a}>${ab_full_names[a]} (${scores[a].mod})</option>`).join(``);
+const dialog_content = `<div><h2>${actor.name}: Ability Check!</h2></div><div>Select Ability:<select name="ability">${dialog_options}</select></div>`;
 
 new Dialog({
   title: "PbtA Ability Check",
@@ -18,13 +19,15 @@ new Dialog({
 async function myCallback(html) {
     const value = html.find("[name=ability]")[0].value;
     const ab_mod = scores[value].mod;
-    console.log(ab_mod);
+
     let roll = await new Roll(`2d6`).roll();
     let message = `<h2><b>${actor.name}</b> attempts a <b>${value.toUpperCase()}</b> ability check!</h2>`;
-    if((roll.total + ab_mod) < 7){message += " failure";}
-    else if((roll.total + ab_mod) > 9){message += " full success";}
-    else{message += " partial success";}
-    message += `, rolled a ${(roll.total+ab_mod)} <i>(${roll.total} + ${ab_mod})</i>`;
+
+    if((roll.total + ab_mod) < 7){message += ` <div style="color:red">Failure</div>`;}
+    else if((roll.total + ab_mod) > 9){message += ` <div style="color:green">Full Success</div>`;}
+    else{message += " <div>Partial Success</div>";}
+
+    message += `rolled a ${(roll.total+ab_mod)} <i>(${roll.total} + ${ab_mod})</i>`;
 
     ChatMessage.create({user:game.user._id, speaker:ChatMessage.getSpeaker({token:actor}),content:message});
 }
